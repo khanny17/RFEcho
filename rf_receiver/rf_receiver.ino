20283c943d0b7be0d7275c96f51741512e3677a9
Kevin <kph1958@rit.edu>
2016-11-02 17:58:00 -0400

    Adding initial code

---
+++ rf_receiver/rf_receiver.ino
@@ -0,0 +1,31 @@
+/*
+  Simple example for receiving
+  
+  http://code.google.com/p/rc-switch/
+  
+  Need help? http://forum.ardumote.com
+*/
+
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+  Serial.begin(9600);
+  mySwitch.enableReceive(0);  // Receiver on inerrupt 0 => that is pin #2
+}
+
+void loop() {
+  if (mySwitch.available()) {
+    
+    int value = mySwitch.getReceivedValue();
+    
+    if (value == 0) {
+      Serial.println("Unknown encoding");
+    } else {
+      Serial.println( mySwitch.getReceivedValue() );
+    }
+
+    mySwitch.resetAvailable();
+  }
+}
